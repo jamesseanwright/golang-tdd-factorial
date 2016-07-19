c02fc6fcbb437295b234bdfab9e315e7c81499d1
James Wright <james@jamesswright.co.uk>
2016-07-19 21:26:44 +0100

    Satisfying tests with for loop implementation

--- getFactorial.go
+++ getFactorial.go
@@ -1,5 +1,19 @@
 package main
 
 func GetFactorial(n int) (int) {
-	return -1
+	if n < 0 {
+		return 0
+	}
+
+	if n <= 1 {
+		return 1
+	}
+
+	factorial := n
+
+	for i := n - 1; i > 0; i-- {
+		factorial *= i
+	}
+
+	return factorial
 }
